2f4add16f182fa070e583d54ccafcb69a27c0aa8
Francesco <franc.ns@gmail.com>
2019-10-16 15:30:15 +0200

    added password gen with js

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// variabili
+var name;
+var surname;
+var color;
+var password;
+var message;
+
+// input utente
+name = prompt('Inserisci il tuo nome');
+surname = prompt('Inserisci il tuo cognome');
+color = prompt('Inserisci il tuo colore preferito');
+
+// password gen
+password = name + surname + color +'19';
+
+// message gen
+message = 'Ciao ' + name + ' ' + surname + ' la tua password è:'
+
+// output
+document.getElementById('js-message').innerHTML = (message);
+document.getElementById('js-password').innerHTML = (password);
